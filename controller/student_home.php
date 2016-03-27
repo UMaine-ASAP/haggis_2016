4ef6b579f43c81ef11285e0a0df2effe7162569a
SLeighton <steven.leighton@maine.edu>
2016-03-26 21:46:11 -0400

    Added simple logout, fixed student_home

--- controller/student_home.php
+++ controller/student_home.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 	//get user file
 	require_once dirname(__FILE__) . "/../models/user.php";
 	require_once dirname(__FILE__) . "/../models/assignment.php";
@@ -75,8 +74,4 @@
 	$page = str_replace('$evaluationsToDo', $evaluationsToDo, $page);
 	$page = str_replace('$evaluationsReceived', $evaluationsReceived, $page);
 	echo $page;
-=======
-session_start();
-echo 'Student login success';
->>>>>>> master
 ?>
