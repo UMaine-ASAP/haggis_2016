ffff85de6deb6fc232e74e44461c6e48f252db5d
Ashley Duggan <ashduggan@comcast.net>
2016-03-29 10:32:49 -0400

    Revert "Merge branch 'master' into Ethan"
    This reverts commit 72ec1b41edc41a18ff1dd707b3db94742e4abe93, reversing
    changes made to 8188d51b969e0e6ec26ea524f5febadcff838834.

--- controller/instructor_home.php
+++ controller/instructor_home.php
@@ -1,17 +1,8 @@
 <?php
-
-	require_once dirname(__FILE__) . "/../models/user.php";
-	require_once dirname(__FILE__) . "/../models/assignment.php";
-	require_once dirname(__FILE__) . "/../models/class.php";
-
 	session_start();
 	if($_SESSION['sessionCheck'] != 'true'){
 			session_destroy();
 			header("location:login.php");
 		}
 	echo 'Success';
-
-    $page = file_get_contents(dirname(__FILE__) . '/../views/instructor_home.html');
-
-    $className .= "hello"; 
 ?>
