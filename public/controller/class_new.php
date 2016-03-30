45f3a74064fa719300c36412c401c1be10f6c926
laviethan49 <laviethan49@gmail.com>
2016-03-30 13:36:54 -0400

    Updated Create A Class, look at comments for finding the course
    I fixed it. Comment on line 18 of class_new.php is as follows:
    
    //This is the only course there is at this point in time, it is
    necessary to change it later, but that is on instructor_home.php's side,
    not mine, I will change it accordingly when we update by uncommenting
    the following line and commenting or removing the previous line.
    
    // $course = $_POST['courseID'];

--- public/controller/class_new.php
+++ public/controller/class_new.php
@@ -1,6 +1,7 @@
 <?php
 	require_once __DIR__ . "/../../system/bootstrap.php";
 	require_once __DIR__ . "/../../models/course.php";
+	require_once __DIR__ . "/../../models/class.php";
 	ensureLoggedIn();
 
 	if($_SESSION['user']->userType == 'Student')
@@ -8,7 +9,12 @@
 		header("location:login.php");
 	}
 
-	$course = $_POST['courseID'];
+	$course = new Course(1);
+
+	//This is the only course there is at this point in time, it is necessary to change it later, but that is on instructor_home.php's side, not mine, I will change it accordingly when we update by uncommenting the following line and commenting or removing the previous line.
+
+	// $course = $_POST['courseID'];
+
 	$courseName = $course->title;
 
 	function createClass()
@@ -17,6 +23,9 @@
 		$classTime = $_POST['classTime'];
 		$classDesc = $_POST['classDesc'];
 		$classLoc = $_POST['classLoc'];
+
+		global $course;
+		$courseIdentifier = $course->courseID;
 
 		$class = new Period(0);
 
@@ -29,6 +38,12 @@
 		$class->addUser($_SESSION['user']->userID, $_SESSION['user']->userType);
 	}
 
+	if(isset($_POST['createClassSubmit']))
+	{
+		createClass();
+		header("location:instructor_home.php");
+	}
+
 	//get the html page ready to be displayed
 	// $page = file_get_contents(dirname(__FILE__) . '/../views/class_new.html');
 
@@ -36,6 +51,6 @@
 	echo $twig->render('class_new.html', [
 		"username"        => $_SESSION['user']->firstName . " " . $_SESSION['user']->lastName,
 		"courseName"         => $courseName,
-		"courseID"			=> $course
+		"courseID"			=> $course->courseCode
 	]);
 ?>
