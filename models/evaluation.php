f21d2d2213c0bc2fec86cc9c5ce8d411fe089c5a
laviethan49 <laviethan49@gmail.com>
2016-03-29 22:05:10 -0400

    Did Things With Stuff and Made Some of it Work

--- models/evaluation.php
+++ models/evaluation.php
@@ -42,7 +42,7 @@ class Evaluation {
 			if($evaluation != NULL){
 				$this->criteriaID = $evaluation['criteriaID'];
 				$this->done = $evaluation['done'];
-				$this->comment = $evaluation['comment'];
+				$this->comment = $evaluation['evaluation_type'];
 				$this->target_userID = $evaluation['target_userID'];
 				$this->evaluation_type = $evaluation['evaluation_type'];
 				$this->groupID = $evaluation['groupID'];
