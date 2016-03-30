ba2f6ad629ce4c258901296974c82641d43f800a
laviethan49 <laviethan49@gmail.com>
2016-03-30 12:22:50 -0400

    Im making a new branch to make it easier after this
    Too many conflicts at this point because so much was changed

---
+++ controller/login.php
@@ -0,0 +1,35 @@
+<?php
+	require_once __DIR__ . "/../system/bootstrap.php";
+
+	//begin session
+	session_start();
+
+	//get user file
+	require_once dirname(__FILE__) . "/../models/user.php";
+
+
+
+	echo $twig->render('login.html');
+
+	//get the html page ready to be displayed
+	if(isset($_POST['submitLogin'])){	//change submitLogin to the equivalent login.html file
+
+		$user = new User(-1); //User with no user id to give
+		$user->Login($_POST['postName'], $_POST['postPassword']); //check for right credentials
+
+		//if correct credentials, set SESSION variables and go to correct home page
+		if($user->userID != -1){
+			$_SESSION['user'] = $user;
+			$_SESSION['sessionCheck'] = 'true';
+			if ($_SESSION['user']->userType == 'Student'){
+				header("location:student_home.php");
+			}
+			else{
+				header("location:instructor_home.php");
+			}
+		}
+		else {
+			echo "Wrong Username/Password</br>Please try again.</br>";
+		}
+	}
+?>
