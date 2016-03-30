7236050ee644659420b6a02b963e26fb1f232280
Ashley Duggan <ashduggan@comcast.net>
2016-03-29 20:35:10 -0400

    Created a Create Assignment view
    View created for Create Assignment as well as Create Evaluation

--- system/database.php
+++ system/database.php
@@ -1,7 +1,7 @@
 <?php 
 
 function GetDB(){
-	$db = new mysqli('localhost', 'root', 'AsAp4U8u', 'projecteval');
+	$db = new mysqli('localhost', 'root', 'projecteval', 'projecteval');
 
 	if($db->connect_errno > 0){
 	    die('Unable to connect to database [' . $db->connect_error . ']');
