b060ff2927abbdf0be8ad79be0189227bd479395
matthew-loewen <matthew.loewen@maine.edu>
2016-03-29 23:45:42 -0400

    working on assigning students
    tim told me to

---
+++ controller/assign_students_to_evaluations.php
@@ -0,0 +1,33 @@
+<?php
+require_once __DIR__ . "/../system/bootstrap.php";
+
+//begin session
+session_start();
+
+echo $twig->render('assign_students_to_evaluations.html', [
+	"professorName"=>"Mike Scott",
+	"title"=>"Edutainment",
+	"description"=>"Create a project with scratch",
+	"students"=>array(
+		array(
+			"firstName"=>"Matthew",
+			"MI"=>"M",
+			"lastName"=>"Loewen"
+			),
+		array(
+			"firstName"=>"Ethan",
+			"MI"=>"J",
+			"lastName"=>"Daville"
+			),
+		array(
+			"firstName"=>"Barby",
+			"MI"=>"M",
+			"lastName"=>"Roberts"
+			),
+		array(
+			"firstName"=>"Dylan",
+			"MI"=>"L",
+			"lastName"=>"Scott"
+			)
+		),
+	]);
