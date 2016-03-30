7ad7328b07ba404acf68dca86038012aea4a14fa
Tim Westbaker <timbone945@gmail.com>
2016-03-29 23:59:28 -0400

    updates stylesheets to use WEB_ROOT environmental variable

--- readme.md
+++ readme.md
@@ -6,3 +6,9 @@
 ##Notes
 - Don't have composer?
 `curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer`
+
+#Environmental Variables
+- DB_DATABASE: Database name
+- DB_USERNAME: Database username
+- DB_PASSWORD: Database password
+- WEB_ROOT: Root of the project. If the web root isn't / set this as appropriate
--- system/bootstrap.php
+++ system/bootstrap.php
@@ -9,14 +9,16 @@ require_once __DIR__ . "/../models/class.php";
 require_once __DIR__ . "/../models/criteria.php";
 require_once __DIR__ . "/../models/evaluation.php";
 
+// Load .env file
+$dotenv = new Dotenv\Dotenv(__DIR__ . "/..");
+$dotenv->load();
 
 // Load the application
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/../views/');
 $twig = new Twig_Environment($loader);
-
-// Load .env file
-$dotenv = new Dotenv\Dotenv(__DIR__ . "/..");
-$dotenv->load();
+if (getenv('WEB_ROOT') != null) {
+	$twig->addGlobal('WEB_ROOT', getenv('WEB_ROOT'));
+}
 
 // Start session
 session_start();
