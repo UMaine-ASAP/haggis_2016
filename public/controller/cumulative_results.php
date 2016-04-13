1483961a605c2aad2e59178c931273ebf70787f7
Benjamin Carlson <carlson.j.ben@gmail.com>
2016-04-13 18:10:18 -0400

    Shows correct averages

--- public/controller/cumulative_results.php
+++ public/controller/cumulative_results.php
@@ -14,34 +14,43 @@ ini_set('display_errors', '1');
 	foreach($assignments as $a){
 		$overallGroup = array();
 		$countGroup = 0;
+		$criteriaCountGroup = 0;
+
 		$overallPeer = array();
 		$countPeer = 0;
+		$criteriaCountPeer = 0;
+
 		$overallIndividual = array();
 		$countIndividual = 0;
+		$criteriaCountIndividual = 0;
 
 		foreach($rec_evals as $eval){
 			$assignmentID = $eval->GetAssignment()->assignmentID;
 			if($a[0]->assignmentID == $assignmentID){
 				if($eval->evaluation_type == 'Group'){
 					$criteria = $eval->GetCriteria();
-					for($i = 0; $i < count($criteria)-1; $i++){
-						$overallGroup[$i][] = $criteria[$i]->GetCriteriaRating($eval->evaluationID);
+					$overallGroup[] = array();
+					$criteriaCountGroup = count($criteria);
+					for($i = 0; $i < count($criteria); $i++){
+						$overallGroup[$countGroup][] += $criteria[$i]->GetCriteriaRating($eval->evaluationID);
 					}
 					$countGroup++;
-					
 				}
 				else if($eval->evaluation_type == 'Peer'){
 					$criteria = $eval->GetCriteria();
-					for($i = 0; $i < count($criteria)-1; $i++){
-						$overallPeer[$i] += $criteria[$i]->GetCriteriaRating($eval->evaluationID);
+					$overallPeer[] = array();
+					$criteriaCountPeer = count($criteria);
+					for($i = 0; $i < count($criteria); $i++){
+						$overallPeer[$countPeer][] += $criteria[$i]->GetCriteriaRating($eval->evaluationID);
 					}
 					$countPeer++;
 				}
 				else if($eval->evaluation_type == 'Individual'){
 					$criteria = $eval->GetCriteria();
-				
+					$overallIndividual[] = array();
+					$criteriaCountIndividual = count($criteria);
 					for($i = 0; $i < count($criteria); $i++){
-						$overallIndividual[$i] += $criteria[$i]->GetCriteriaRating($eval->evaluationID);
+						$overallIndividual[$countIndividual][] += $criteria[$i]->GetCriteriaRating($eval->evaluationID);
 					}
 					$countIndividual++;
 				}
@@ -49,39 +58,65 @@ ini_set('display_errors', '1');
 		}
 		if($countGroup > 0){
 			$criteriaGroupFinal = array();
-			foreach ($overallGroup as $criteria) {
-				var_dump($criteria);echo"<br>";
+			for ($i=0; $i < $criteriaCountGroup; $i++) { 
+				$criteriaGroupFinal[] = 0;
 			}
+			foreach ($overallGroup as $criteria) {
+				for ($i=0; $i < $criteriaCountGroup; $i++) { 
+					$criteriaGroupFinal[$i] += $criteria[$i];
+				}
+			}
+			for ($i=0; $i < $criteriaCountGroup; $i++) { 
+				$criteriaGroupFinal[$i] = round($criteriaGroupFinal[$i]/$countGroup,1);
+			}
+
 			$assignment_results[] = [
 				"name"		=> $a[0]->title . " Group Results",
 				"id"		=> $a[0]->assignmentID,
-				"criteria" => $criteriaFinal,
-				"resultTwo" => round($overallGroup[1]/$countGroup,1),
-				"resultThree" => round($overallGroup[2]/$countGroup,1),
-				"resultFour" => round($overallGroup[3]/$countGroup,1),
-				"resultFive" => round($overallGroup[4]/$countGroup,1)
+				"criteria" => $criteriaGroupFinal,
+				"criteriaCount" => $criteriaCountGroup
 			];
 		}
 		if($countPeer > 0){
+			$criteriaPeerFinal = array();
+			for ($i=0; $i < $criteriaCountPeer; $i++) { 
+				$criteriaPeerFinal[] = 0;
+			}
+			foreach ($overallPeer as $criteria) {
+				for ($i=0; $i < $criteriaCountPeer; $i++) { 
+					$criteriaPeerFinal[$i] += $criteria[$i];
+				}
+			}
+			for ($i=0; $i < $criteriaCountPeer; $i++) { 
+				$criteriaPeerFinal[$i] = round($criteriaPeerFinal[$i]/$countPeer,1);
+			}
+
 			$assignment_results[] = [
 				"name"		=> $a[0]->title . " Peer Results",
 				"id"		=> $a[0]->assignmentID,
-				"resultOne" => round($overallPeer[0]/$countPeer,1),
-				"resultTwo" => round($overallPeer[1]/$countPeer,1),
-				"resultThree" => round($overallPeer[2]/$countPeer,1),
-				"resultFour" => round($overallPeer[3]/$countPeer,1),
-				"resultFive" => round($overallPeer[4]/$countPeer,1)
+				"criteria" => $criteriaPeerFinal,
+				"criteriaCount" => $criteriaCountPeer
 			];
 		}
 		if($countIndividual > 0){
+			$criteriaIndividualFinal = array();
+			for ($i=0; $i < $criteriaCountIndividual; $i++) { 
+				$criteriaIndividualFinal[] = 0;
+			}
+			foreach ($overallIndividual as $criteria) {
+				for ($i=0; $i < $criteriaCountIndividual; $i++) { 
+					$criteriaIndividualFinal[$i] += $criteria[$i];
+				}
+			}
+			for ($i=0; $i < $criteriaCountIndividual; $i++) { 
+				$criteriaIndividualFinal[$i] = round($criteriaIndividualFinal[$i]/$countIndividual,1);
+			}
+
 			$assignment_results[] = [
 				"name"		=> $a[0]->title . " Individual Results",
 				"id"		=> $a[0]->assignmentID,
-				"resultOne" => round($overallIndividual[0]/$countIndividual,1),
-				"resultTwo" => round($overallIndividual[1]/$countIndividual,1),
-				"resultThree" => round($overallIndividual[2]/$countIndividual,1),
-				"resultFour" => round($overallIndividual[3]/$countIndividual,1),
-				"resultFive" => round($overallIndividual[4]/$countIndividual,1)
+				"criteria" => $criteriaIndividualFinal,
+				"criteriaCount" => $criteriaCountIndividual
 			];
 		}
 	}
