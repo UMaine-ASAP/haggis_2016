9efebb0e8b4d4dc81b2a58ad6d40d7871609fb3c
matthew-loewen <matt@pasa.asap.um.maine.edu>
2016-04-13 18:56:13 -0400

    add students to groups

--- public/controller/instructor_assignment.php
+++ public/controller/instructor_assignment.php
@@ -2,6 +2,10 @@
 	require_once __DIR__ . "/../../system/bootstrap.php";
 	ensureLoggedIn();
 
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
 	//grab assignment info
 	if(isset($_POST['assignmentID'])){
 		$_SESSION['assignmentID'] =  $_POST['assignmentID'];
@@ -10,6 +14,7 @@
 		$master_evaluations = $assignment->GetEvaluations();
 		$class = $assignment->GetClasses()[0]; //class for this assignment
 		$all_users = $class->GetUsers();
+		$Get_Groups = $assignment->GetGroups();
 		
 	}
 	else
@@ -22,7 +27,22 @@
 	$all_peer_evals = $master_evaluations[1]->GetChildEvaluations();
 	$evalsTotal = count($all_group_evals);
 	$evalsTotal += count($all_peer_evals);
-	$students = 
+	$students = array();
+	$groups = array();	
+
+	//adds EVERY STUDENT into the $studens array
+	foreach($all_users as $user){ //
+		if ($user->userType == "Student"){
+			$students[] = ["name"=>$user->firstName." ".$user->lastName];
+		}
+	}
+
+	//adds existing groups into the $groups array 
+	foreach($Get_Groups as $group){
+		$groups[]= ["groupID"=>$group->student_groupID, "number"=>$group->groupNumber];
+	}
+
+
 
 	$instructor = $_SESSION['user']->userID;
 
@@ -35,8 +55,8 @@
 			"assignmentID" => $_SESSION['assignmentID'],
 			"groupEvaluationID" => $master_evaluations[0]->evaluationID,
 			"peerEvaluationID" => $master_evaluations[1]->evaluationID,
-			"students"			=>   					,
-			"groups"			=> 
+			"students" => $students,     //array(["name"=>'matt', "userID"=>50],["name"=>'steven'],["name"=>'matt']),
+			"groups" => $groups           //array(["groupID"=>'6', "number"=>'1'])
 		]);
 
 ?>
--- public/controller/login.php
+++ public/controller/login.php
@@ -1,5 +1,4 @@
-<?php
-
+<?php 
 	require_once __DIR__ . "/../../system/bootstrap.php";
 
 	if (isset($_GET['message'])) {
