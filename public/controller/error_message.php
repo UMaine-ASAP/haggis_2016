c9587608d37b996befa2f58912dcdbcd3487052c
laviethan49 <laviethan49@gmail.com>
2016-04-25 16:34:21 -0400

    Added functionality for nicer looking errors that tell us what happened
    The error codes are going to be defined in error_codes.php on the main
    folder. The php errors don't look nice because of the way that an error
    handler has to be defined.

--- public/controller/error_message.php
+++ public/controller/error_message.php
@@ -1,10 +1,20 @@
 <?php
 require_once __DIR__ . "/../../system/bootstrap.php";
 
+if(isset($_GET['error']))
+	$errNum = $_GET['error'];
+else
+	$errNum = 0;
+
+if(isset($_GET['error-detailed']))
+	$errDet = $_GET['error-detailed'];
+else
+	$errDet = " No Such Error Known ";
+
 echo $twig->render('error_message.html',
 	[
 	"username"            => $_SESSION['user']->firstName . " " . $_SESSION['user']->lastName,
-	"errorNum"         	  => $_GET['error'],
-	"errorMessage"        => $_GET['error-detailed']
+	"errorNum"         	  => $errNum,
+	"errorMessage"        => $errDet
 	]);
 ?>
--- public/controller/redirect.php
+++ public/controller/redirect.php
@@ -3,7 +3,7 @@ require_once __DIR__ . "/../../system/bootstrap.php";
 
 ensureLoggedIn();
 
-if(isset($_GET['error']) && isset($_GET['error-detailed']))
+if(isset($_GET['error']) || isset($_GET['error-detailed']))
 {
 	header("location:error_message.php");
 }
