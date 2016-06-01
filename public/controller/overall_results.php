2c5b0d4fd7c0bfb7be00b80ddf8869b72bb60ec7
Benjamin Carlson <carlson.j.ben@gmail.com>
2016-06-01 17:02:23 -0400

    Overall Results progress
    This is a working version of the overall results. It contains changes
    to the PHP file to create requisite draws from the database and data
    calculation. The HTML file comments these changes for front-end work.
    NOTE: THIS IS NOT THE COMPLETED VERSION!

--- public/controller/overall_results.php
+++ public/controller/overall_results.php
@@ -5,20 +5,45 @@
 	ensureLoggedIn();
 
 	############Data Processing############
+	#We want a 3D array of [student][criteria][rating]
 
-	#Declarations
+	#Finding data in database
 	$assignment = new Assignment($_SESSION['assignmentID']);
+	$class = new Period($_SESSION['classID']);
+	$students = $class->GetUsers();
 	$evaluations = $assignment->GetEvaluations();
-	$criteria = $assignment->GetCriteria();
+	$criteria = $evaluations[0]->GetCriteria();
 
-	echo '<pre>' . print_r($_SESSION, TRUE) . '</pre>';
-	echo '<pre>' . print_r($_POST, TRUE) . '</pre>';
+	#Removing teachers from the students
+	foreach ($students as $student){
+		if ($student->userType == 'Instructor'){
+			unset($students[array_search($student, $students)]);
+		}
+	}
+
+	#Creating an array for the relevant information
+	#This array is structured as [studentID][criteriaID][rating/comments]
+	$assignmentData = array();
+	foreach ($students as $student){
+		$assignmentData[$student->userID] = array();
+		foreach ($criteria as $criterion){
+			$assignmentData[$student->userID][$criterion->criteriaID] = array();
+			$assignmentData[$student->userID][$criterion->criteriaID]['rating'] = 0;
+			$assignmentData[$student->userID][$criterion->criteriaID]['comments'] = 0;
+		}
+	}
+
+	#enable each of these to see important information
+	#echo '<pre>' . print_r($_SESSION, TRUE) . '</pre>';
+	#echo '<pre>' . print_r($_POST, TRUE) . '</pre>';
+	#echo '<pre>' . print_r($assignmentData, TRUE) . '</pre>';
 
 	############Rendering page############
 	echo $twig->render('overall_results.html', [
 		"username"        => $_SESSION['user']->firstName . " " . $_SESSION['user']->lastName,
 		"evaluations"     => $evaluations,
 		"assignment"	  => $assignment,
-		"criteria"		  => $criteria
+		"criteria"		  => $criteria,
+		"assignmentData"  => $assignmentData
 	]);
 ?>
