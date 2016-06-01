4fe4f89b85429fd14ce41baf4ab0920b9d4807aa
Benjamin Carlson <carlson.j.ben@gmail.com>
2016-06-01 14:35:48 -0400

    Progress for collaboration
    This is a progress marker so Matt can start working on the HTML file
    while I work on the PHP file.

--- public/controller/overall_results.php
+++ public/controller/overall_results.php
@@ -1,15 +1,24 @@
 <?php
 
-	#Inclusions
+	############Inclusions############
 	require_once __DIR__ . "/../../system/bootstrap.php";
 	ensureLoggedIn();
 
-	#Data Processing
+	############Data Processing############
+
+	#Declarations
+	$assignment = new Assignment($_SESSION['assignmentID']);
+	$evaluations = $assignment->GetEvaluations();
+	$criteria = $assignment->GetCriteria();
+
 	echo '<pre>' . print_r($_SESSION, TRUE) . '</pre>';
 	echo '<pre>' . print_r($_POST, TRUE) . '</pre>';
 
-	#Rendering page
+	############Rendering page############
 	echo $twig->render('overall_results.html', [
 		"username"        => $_SESSION['user']->firstName . " " . $_SESSION['user']->lastName,
+		"evaluations"     => $evaluations,
+		"assignment"	  => $assignment
+		"criteria"		  => $criteria;
 	]);
 ?>
