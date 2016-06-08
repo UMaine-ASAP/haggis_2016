e65a383e45635a66950c0538bcf23edab1ade141
Benjamin Carlson <carlson.j.ben@gmail.com>
2016-06-08 10:51:48 -0400

    Updated password reset
    I updated the password reset in user

--- public/controller/passwordreset.php
+++ public/controller/passwordreset.php
@@ -13,13 +13,13 @@ echo $twig->render('passwordreset.html', [
 if(isset($_POST['postEmail'])){
 	if($_POST['postEmail'] != null){
 		echo "<br>The account associated with ".$_POST['postEmail']." has had its password reset. Please check your email for your new password.<br>";
-		$returnFromFunction = User::ResetPassword();
-		if (mail('matthewlanimates@gmail.com','Obama your lord and savior', 'Yeah this is just a test','matt@matt.com')){
+		echo $returnFromFunction = User::ResetPassword();
+		/*if (mail('matthewlanimates@gmail.com','Obama your lord and savior', 'Yeah this is just a test','matt@matt.com')){
 			echo 'hello';
 		}
 		else{
 			echo 'dammit';
-		}
+		}*/
 	}else{
 		echo "<br>Your email address is required to reset your password.";
 	}
